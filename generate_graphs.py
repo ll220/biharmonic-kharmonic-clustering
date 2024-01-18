e3d6d808444623baa2fdf491ef1c4665ee2a7e97
blackmit <blackmit@oregonstate.edu>
2024-01-18 08:34:54 -0800

    Put the code to generate the iris knn graph into a method

--- generate_graphs.py
+++ generate_graphs.py
@@ -5,20 +5,23 @@ import random
 from sklearn.datasets import load_iris
 from sklearn.neighbors import kneighbors_graph
 
+def load_iris_graph_and_labels(num_neighbors : int) -> nx.Graph:
+    data = load_iris()
+    labels = data.target
+    points = np.array(data.data)
+    adjacency_matrix = kneighbors_graph(points, num_neighbors, mode='connectivity', include_self=False)
+    graph = nx.from_numpy_array(adjacency_matrix)
+    return graph, labels
 
-data = load_iris()
-labels = data.target
-points = np.array(data.data)
 
-adjacency_matrix = kneighbors_graph(points, 125, mode='connectivity', include_self=False)
-graph = nx.from_numpy_array(adjacency_matrix)
-
-colors = ['red', 'blue', 'green']
-node_colors = [colors[cluster] for cluster in labels]
-nx.draw(graph, with_labels=True, node_color=node_colors)
-plt.title('Iris Generated Graph k=125')
-plt.savefig('Iris Generated Graph k=125')
-plt.show()
+if __name__=="__main__":
+    graph, labels = load_iris_graph_and_labels(num_neighbors=125)
+    colors = ['red', 'blue', 'green']
+    node_colors = [colors[cluster] for cluster in labels]
+    nx.draw(graph, with_labels=True, node_color=node_colors)
+    plt.title('Iris Generated Graph k=125')
+    plt.savefig('Iris Generated Graph k=125')
+    plt.show()
 
 # sizes = [75, 75, 300]
 # # probs = [[0.25, 0.05, 0.02], [0.05, 0.35, 0.07], [0.02, 0.07, 0.40]]
