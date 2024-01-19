315ca75af5ed839cab9117e1edf64cb69b8b5f50
blackmit <blackmit@oregonstate.edu>
2024-01-18 16:03:03 -0800

    added wine and breast cancer to the list of graphs

--- generate_graphs.py
+++ generate_graphs.py
@@ -2,25 +2,38 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, load_wine, load_breast_cancer
 from sklearn.neighbors import kneighbors_graph
 
 def load_iris_graph_and_labels(num_neighbors : int) -> nx.Graph:
-    data = load_iris()
-    labels = data.target
-    points = np.array(data.data)
+    data, labels = load_iris(return_X_y=True)
+    points = np.array(data)
+    adjacency_matrix = kneighbors_graph(points, num_neighbors, mode='connectivity', include_self=False)
+    graph = nx.from_numpy_array(adjacency_matrix)
+    return graph, labels
+
+def load_wine_graph_and_labels(num_neighbors : int) -> nx.Graph:
+    data, labels = load_wine(return_X_y=True)
+    points = np.array(data)
+    adjacency_matrix = kneighbors_graph(points, num_neighbors, mode='connectivity', include_self=False)
+    graph = nx.from_numpy_array(adjacency_matrix)
+    return graph, labels
+
+def load_cancer_graph_and_labels(num_neighbors : int) -> nx.Graph:
+    data, labels = load_breast_cancer(return_X_y=True)
+    points = np.array(data)
     adjacency_matrix = kneighbors_graph(points, num_neighbors, mode='connectivity', include_self=False)
     graph = nx.from_numpy_array(adjacency_matrix)
     return graph, labels
 
 
 if __name__=="__main__":
-    graph, labels = load_iris_graph_and_labels(num_neighbors=125)
+    graph, labels = load_cancer_graph_and_labels(num_neighbors=25)
     colors = ['red', 'blue', 'green']
     node_colors = [colors[cluster] for cluster in labels]
     nx.draw(graph, with_labels=True, node_color=node_colors)
     plt.title('Iris Generated Graph k=125')
-    plt.savefig('Iris Generated Graph k=125')
+    # plt.savefig('Iris Generated Graph k=125')
     plt.show()
 
 # sizes = [75, 75, 300]
