4722dad649709de68a2d01ee21687f5e4a2cbea8
blackmit <blackmit@oregonstate.edu>
2024-01-26 09:22:50 -0800

    updated spectral clustering to accept a variable number of clusters

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -30,65 +30,63 @@ def run_combined_k_harmonic_spectral_clustering(graph, k_harmonic):
 
     return kmeans.labels_
 
-def run_spectral_clustering(graph):
+def run_spectral_clustering(
+    graph : nx.Graph,
+    num_clusters : int
+):
     laplacian = nx.laplacian_matrix(graph)
     laplacian = laplacian.toarray()
-
     _, eigenvectors = np.linalg.eigh(laplacian)
-    first_k_eigenvectors = eigenvectors[:, 1:4]
-    # first_k_eigenvectors = eigenvectors
-    # first_k_eigenvectors = first_k_eigenvectors.T
-    # print(first_k_eigenvectors)
-
-    kmeans = KMeans(n_clusters=3, random_state=0, n_init="auto").fit(first_k_eigenvectors)
-
+    first_k_eigenvectors = eigenvectors[:, 1:num_clusters+1]
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0, n_init="auto").fit(first_k_eigenvectors)
     return kmeans.labels_
 
-K_HARMONICS = [0.1, 0.5, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 50, 100]
+if __name__=="__main__":
+    K_HARMONICS = [0.1, 0.5, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 50, 100]
 
-# for k_neighbors in [25, 50, 75, 100, 125]:
-#     f = open("sc_k_harmonic_combined_" + str(k_neighbors) + "_neighbors.txt", "w")
-#     G, true_clusters = load_iris_graph_and_labels(num_neighbors=k_neighbors)
-#     mean_accuracies = []
+    # for k_neighbors in [25, 50, 75, 100, 125]:
+    #     f = open("sc_k_harmonic_combined_" + str(k_neighbors) + "_neighbors.txt", "w")
+    #     G, true_clusters = load_iris_graph_and_labels(num_neighbors=k_neighbors)
+    #     mean_accuracies = []
 
-#     for k_harmonic in K_HARMONICS:
-#         accuracies = []
-#         for _ in range(10):
-#             results = run_combined_k_harmonic_spectral_clustering(G, k_harmonic)
-#             purity = get_purity(results, true_clusters)
-#             accuracies.append(purity)
+    #     for k_harmonic in K_HARMONICS:
+    #         accuracies = []
+    #         for _ in range(10):
+    #             results = run_combined_k_harmonic_spectral_clustering(G, k_harmonic)
+    #             purity = get_purity(results, true_clusters)
+    #             accuracies.append(purity)
 
-#         mean_accuracy = np.mean(accuracies)
-#         mean_accuracies.append(mean_accuracy)
-#         f.write("k harmonic=" + str(k_harmonic) + " : " + str(mean_accuracy) + "\n")
+    #         mean_accuracy = np.mean(accuracies)
+    #         mean_accuracies.append(mean_accuracy)
+    #         f.write("k harmonic=" + str(k_harmonic) + " : " + str(mean_accuracy) + "\n")
 
-#     plt.plot(K_HARMONICS, mean_accuracies)
-#     plt.title(str(k_neighbors) + " neighbors " + "k harmonic vs mean purity")
-#     plt.savefig(str(k_neighbors) + " neighbors " + "k harmonic vs mean purity")
-#     plt.clf()
-#     f.close()
+    #     plt.plot(K_HARMONICS, mean_accuracies)
+    #     plt.title(str(k_neighbors) + " neighbors " + "k harmonic vs mean purity")
+    #     plt.savefig(str(k_neighbors) + " neighbors " + "k harmonic vs mean purity")
+    #     plt.clf()
+    #     f.close()
 
-f = open("spectral_clustering_purities.txt", "w")
-for k_neighbors in [25, 50, 75, 100, 125]:
-    G, true_clusters = load_iris_graph_and_labels(num_neighbors=k_neighbors)
-    
-    accuracies = []
-    for i in range(10):
-        labels = run_spectral_clustering(G)
-        purity = get_purity(labels, true_clusters)
-        accuracies.append(purity)
+    f = open("spectral_clustering_purities.txt", "w")
+    for k_neighbors in [25, 50, 75, 100, 125]:
+        G, true_clusters = load_iris_graph_and_labels(num_neighbors=k_neighbors)
+        
+        accuracies = []
+        for i in range(10):
+            labels = run_spectral_clustering(G, num_clusters=3)
+            purity = get_purity(labels, true_clusters)
+            accuracies.append(purity)
 
-        if (i==9):
-            colors = ['red', 'blue', 'green']
-            node_colors = [colors[cluster] for cluster in labels]
-            nx.draw(G, with_labels=True, node_color=node_colors)
-            plt.title('Clustered Graph')        
-            # plt.savefig("spectral clustering_" + str(k))
-            plt.show()    
-            plt.clf()
+            if (i==9):
+                colors = ['red', 'blue', 'green']
+                node_colors = [colors[cluster] for cluster in labels]
+                nx.draw(G, with_labels=True, node_color=node_colors)
+                plt.title('Clustered Graph')        
+                # plt.savefig("spectral clustering_" + str(k))
+                plt.show()    
+                plt.clf()
 
-    mean_accuracy = np.mean(accuracies)
-    f.write(str(k_neighbors) + ":" + str(mean_accuracy) + "\n")
+        mean_accuracy = np.mean(accuracies)
+        f.write(str(k_neighbors) + ":" + str(mean_accuracy) + "\n")
 
 
 
