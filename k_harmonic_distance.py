e53236659f12cfc5e73ed472063c1937438b3dcd
blackmit <blackmit@oregonstate.edu>
2024-05-07 11:37:12 -0700

    edited docstrings of k-harmonic distance

--- k_harmonic_distance.py
+++ k_harmonic_distance.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy as np
 from scipy.linalg import fractional_matrix_power
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 def component_mask(graph : nx.Graph) -> np.array:
     """ Return the component mask of a graph
@@ -9,6 +9,7 @@ def component_mask(graph : nx.Graph) -> np.array:
     The component mask of a graph is a matrix C
     where C[i,j] = 1 if vertices i and j are
     in the same connected component, and 0 otherwise.
+
     """
     components = [list(c) for c in nx.connected_components(graph)]
     component_indicators = np.zeros((len(graph), len(components))) # component_indicators[i,j] = 1 if vertex i in component j else 0
@@ -48,22 +49,41 @@ def max_effective_resistance(graph : nx.Graph) -> Tuple[int,int]:
     max_edge_idx = np.argmax(resistances_of_edges)
     return edges[max_edge_idx]
 
-def biharmonics_of_edges(graph : nx.Graph) -> List[float]:
-    """ Return the edge with the highest biharmonic distance """
+def biharmonics_of_edges(graph : nx.Graph, as_dict : bool=False):
+    """ Return the biharmonic distance of all edges in the graph 
+    
+    Args:
+        graph: NetworkX graph
+        as_dict: If True, return the biharmonic distances as a dict of the form { (int, int) : float}.
+            If False, return the biharmonic distances as a list (in the same order as graph.edges)
+    """
     laplacian = nx.laplacian_matrix(graph).todense()
     pinv = np.linalg.pinv(laplacian, hermitian=True)
     pinv_squared = pinv @ pinv
     biharmonic_distance = lambda s, t : pinv_squared[s,s] + pinv_squared[t,t] - 2*pinv_squared[s,t]
-    biharmonics_of_edges = [biharmonic_distance(u,v) for u,v in graph.edges]
+    if as_dict:
+        biharmonics_of_edges = { (u,v) : biharmonic_distance(u,v) for u,v in graph.edges }
+    else:
+        biharmonics_of_edges = [ biharmonic_distance(u,v) for u,v in graph.edges ]
     return biharmonics_of_edges
 
-def k_harmonics_of_edges(graph : nx.Graph, k : int) -> Tuple[int,int]:
-    """ Return the edge with the highest biharmonic distance """
+def k_harmonics_of_edges(graph : nx.Graph, k : int, as_dict : bool=False) -> Tuple[int,int]:
+    """ Return the k-harmonic distance of all edges in the graph 
+    
+    Args:
+        graph: NetworkX graph
+        k: power of the k-harmonic distance
+        as_dict: If True, return the biharmonic distances as a dict of the form { (int, int) : float}.
+            If False, return the biharmonic distances as a list (in the same order as graph.edges)
+    """
     laplacian = nx.laplacian_matrix(graph).todense()
     pinv = np.linalg.pinv(laplacian, hermitian=True)
     k_pinv = fractional_matrix_power(pinv, float(k))
     k_harmonic_distance = lambda s, t : k_pinv[s,s] + k_pinv[t,t] - 2*k_pinv[s,t]
-    k_harmonic_of_edges = [k_harmonic_distance(u,v) for u,v in graph.edges]
+    if as_dict:
+        k_harmonic_of_edges = {(u,v) : k_harmonic_distance(u,v) for u,v in graph.edges}
+    else:
+        k_harmonic_of_edges = [k_harmonic_distance(u,v) for u,v in graph.edges]
     return k_harmonic_of_edges
 
 def total_resistance(graph : nx) -> float:
