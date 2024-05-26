703a09e85e0e75bd0b14ff70ea3543d1cb508c3d
blackmit <blackmit@oregonstate.edu>
2024-05-26 11:31:34 -0700

    added docstring to generate graphs

--- generate_graphs.py
+++ generate_graphs.py
@@ -1,25 +1,57 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-import random
 from sklearn.datasets import load_iris, load_wine, load_breast_cancer
 from sklearn.neighbors import kneighbors_graph
 
-def load_iris_graph_and_labels(num_neighbors : int) -> nx.Graph:
+def load_iris_graph_and_labels(num_neighbors : int) -> tuple[nx.Graph, np.ndarray[int]]:
+    """ Return the nearest neighbor graph and cluster labels of the Iris dataset 
+    
+    Args:
+        num_neighbors: number of neigbors to use for nearest neighbor graph
+
+    Returns:
+        A tuple containing:
+            graph: the nearest neighbor graph
+            labels: a Numpy array containing the cluster label for each node as an int,
+                e.g. labels = [0,0,0,1,1,1].
+    """
     data, labels = load_iris(return_X_y=True)
     points = np.array(data)
     adjacency_matrix = kneighbors_graph(points, num_neighbors, mode='connectivity', include_self=False)
     graph = nx.from_numpy_array(adjacency_matrix)
     return graph, labels
 
-def load_wine_graph_and_labels(num_neighbors : int) -> nx.Graph:
+def load_wine_graph_and_labels(num_neighbors : int) -> tuple[nx.Graph, np.ndarray[int]]:
+    """ Return the nearest neighbor graph and cluster labels of the Wine dataset 
+    
+    Args:
+        num_neighbors: number of neigbors to use for nearest neighbor graph
+
+    Returns:
+        A tuple containing:
+            graph: the nearest neighbor graph
+            labels: a Numpy array containing the cluster label for each node as an int,
+                e.g. labels = [0,0,0,1,1,1].
+    """
     data, labels = load_wine(return_X_y=True)
     points = np.array(data)
     adjacency_matrix = kneighbors_graph(points, num_neighbors, mode='connectivity', include_self=False)
     graph = nx.from_numpy_array(adjacency_matrix)
     return graph, labels
 
-def load_cancer_graph_and_labels(num_neighbors : int) -> nx.Graph:
+def load_cancer_graph_and_labels(num_neighbors : int) -> tuple[nx.Graph, np.ndarray[int]]:
+    """ Return the nearest neighbor graph and cluster labels of the Breast Cancer dataset 
+    
+    Args:
+        num_neighbors: number of neigbors to use for nearest neighbor graph
+
+    Returns:
+        A tuple containing:
+            graph: the nearest neighbor graph
+            labels: a Numpy array containing the cluster label for each node as an int,
+                e.g. labels = [0,0,0,1,1,1].
+    """
     data, labels = load_breast_cancer(return_X_y=True)
     points = np.array(data)
     adjacency_matrix = kneighbors_graph(points, num_neighbors, mode='connectivity', include_self=False)
@@ -29,7 +61,19 @@ def load_cancer_graph_and_labels(num_neighbors : int) -> nx.Graph:
 def load_uci_graph_and_labels(
     num_neighbors : int, 
     id : int
-):
+) -> tuple[nx.Graph, np.ndarray[int]]:
+    """ Return the nearest neighbor graph and cluster labels of a dataset from the UCI repository
+    
+    Args:
+        num_neighbors: number of neigbors to use for nearest neighbor graph
+        id: the label of the 
+
+    Returns:
+        A tuple containing:
+            graph: the nearest neighbor graph
+            labels: a Numpy array containing the cluster label for each node as an int,
+                e.g. labels = [0,0,0,1,1,1].
+    """
     from ucimlrepo import fetch_ucirepo 
     dataset = fetch_ucirepo(id=id)
     X = dataset.data.features.to_numpy()
@@ -43,8 +87,8 @@ def load_block_stochastic_graph_and_labels(
     num_clusters : int,
     intercluster_p : float,
     intracluster_p : float
-) -> nx.Graph:
-    """ return a block stochatsic graph
+) -> tuple[nx.Graph, np.ndarray[int]]:
+    """ Return a block stochatsic graph
     
     args:
         num_nodes_per_cluster
@@ -55,55 +99,49 @@ def load_block_stochastic_graph_and_labels(
     """
     cluster_sizes = num_clusters * [num_nodes_per_cluster]
     P = (intracluster_p-intercluster_p)*np.eye(num_clusters) + (intercluster_p)*np.ones((num_clusters, num_clusters))
-    graph = nx.stochastic_block_model(
-        cluster_sizes,
-        P
-    )
+    graph = nx.stochastic_block_model(cluster_sizes, P)
     labels = np.array([
         i 
         for i in range(num_clusters)
         for _ in range(num_nodes_per_cluster)
     ])
-
     return graph, labels
 
+def degree_k_tree(k : int, depth : int) -> nx.Graph:
+    """ Return the degree `k` tree with `depth` level """    
+    def circle_position(r : float, theta : float):
+        """ Return the Euclidean coordinates from the radial coordinates """
+        return (r*np.cos(2*np.pi*theta), r*np.sin(2*np.pi*theta))
+    def get_node_index(k : int, level : int, i : int) -> int:
+        """ Return the global node index of the `i` node in `level` """
+        return (1 - k**level)//(1-k) + i
 
-if __name__=="__main__":
-    """
-    graph, labels = load_cancer_graph_and_labels(num_neighbors=25)
-    colors = ['red', 'blue', 'green']
-    node_colors = [colors[cluster] for cluster in labels]
-    nx.draw(graph, with_labels=True, node_color=node_colors)
-    plt.title('Iris Generated Graph k=125')
-    # plt.savefig('Iris Generated Graph k=125')
-    plt.show()
-    """
-    graph, labels = load_uci_graph_and_labels(num_neighbors=10, id=320)
+    graph = nx.Graph()
+    pos = {}
+    pos[0] = (0, 0) # root is at origin
+    for level in range(1, depth + 1):
+        num_nodes_at_level = k**level
+        for i in range(k**depth):
+            node = get_node_index(k, level, i)
+            parent = get_node_index(k, level-1, i // k)
+            pos[node] = circle_position(r=level, theta=i/num_nodes_at_level+1/(2*num_nodes_at_level))
+            graph.add_edge(parent, node)
+    graph.pos = pos
+    return graph
 
-# sizes = [75, 75, 300]
-# # probs = [[0.25, 0.05, 0.02], [0.05, 0.35, 0.07], [0.02, 0.07, 0.40]]
-
-# sizes = [50] * 10
-# # probs = [[0.1, 0.001, 0.001], [0.001, 0.1, 0.001], [0.001, 0.001, 0.1]]
-
-# random_seed = 42
-# seed = np.random.seed(random_seed)
-# g = nx.random_partition_graph(sizes, 1, 0.02, seed=seed)
-# # g = nx.stochastic_block_model(sizes, probs)
-
-
-# # guarantee completedness
-# while not nx.is_connected(g):
-#     component1, component2 = random.sample(list(nx.connected_components(g)), 2)
-#     node1 = random.choice(list(component1))
-#     node2 = random.choice(list(component2))
-    
-#     g.add_edge(node1, node2)
-
-# H = nx.quotient_graph(g, g.graph["partition"], relabel=True)
-# fh = open("./ten_graphs/size_50/100_2.txt", "wb")
-# nx.write_edgelist(g, fh, data=False)
-# nx.draw(g, with_labels = True)
-# nx.draw(H, with_labels = True)
-# # plt.savefig("./graphs/size_fifty_pngs/10_point5.png")
-# plt.show()
+def grid_graph_2d(n : int):
+    """ Return a square (`n` x `n`) grid graph """
+    graph = nx.grid_graph([n,n])
+    map = {
+        (i,j) : i*n + j
+        for i in range(n)
+        for j in range(n)
+    }
+    pos = {
+        i*n+j : (i,j)
+        for i in range(n)
+        for j in range(n)
+    }
+    graph = nx.convert_node_labels_to_integers(graph)
+    graph.pos = pos
+    return graph
